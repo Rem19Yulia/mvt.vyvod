a4f91b8ceeea01dd73e3b84604edbc979a7d925e
Rem19Yulia <kyv190591@gmail.com>
2025-02-06 13:59:48 +0300

    Update admin.py
    сайт администрирования

--- task1/admin.py
+++ task1/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Buyer, Game
 
-admin.site.register(Buyer)
-admin.site.register(Game)
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')  # Отображение полей
+    list_filter = ('size', 'cost')  # Фильтрация по полям
+    search_fields = ('title',)  # Поиск по полю
+    list_per_page = 20  # Ограничение кол-ва записей
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')  # Отображение полей
+    list_filter = ('balance', 'age')  # Фильтрация по полям
+    search_fields = ('name',)  # Поиск по полю
+    list_per_page = 30  # Ограничение кол-ва записей
+    readonly_fields = ('balance',)  # Поле только для чтения
